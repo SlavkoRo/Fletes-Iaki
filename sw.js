022897179cae4493f441d6c497a581db42f0c25d
SlavkoRo <165854034+SlavkoRo@users.noreply.github.com>
2026-05-02 17:58:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,7 @@
-const CACHE_NAME = "fletes-iaki-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon.png"
-];
-
-// Instalación: Guarda los archivos necesarios
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
-  self.skipWaiting();
+ e.waitUntil(caches.open("app").then(c => c.addAll(["./", "index.html", "manifest.json", "icon.png"])));
 });
 
-// Activación: Limpia versiones viejas
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Respuesta: Intenta red primero, si falla va al cache
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+ e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
